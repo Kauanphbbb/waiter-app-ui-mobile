020039b08d60d3c4773335259c1dd9d195e49bba
Kauanphbbb <kauanphbbb@gmail.com>
2024-03-30 20:12:41 -0300

    feat: create menu component

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,3 +1,47 @@
+import { FlatList } from 'react-native';
+import { products } from '../../mocks/products';
+import { formatCurrency } from '../../utils/formatCurrency';
+import { PlusCircle } from '../Icons/PlusCircle';
+import { Text } from '../Text';
+import {
+  AddToCartButton,
+  Product,
+  ProductDetails,
+  ProductImage,
+  Separator,
+} from './styles';
+
 export function Menu() {
-  return null;
+  return (
+    <FlatList
+      data={products}
+      keyExtractor={(product) => product._id}
+      style={{ marginTop: 32 }}
+      ItemSeparatorComponent={Separator}
+      contentContainerStyle={{ paddingHorizontal: 24 }}
+      renderItem={({ item: product }) => (
+        <Product>
+          <ProductImage
+            source={{
+              uri: `http://192.168.1.112:3001/uploads/${product.imagePath}`,
+            }}
+          />
+
+          <ProductDetails>
+            <Text $weight="600">{product.name}</Text>
+            <Text $size={14} $color="#666" style={{ marginVertical: 8 }}>
+              {product.description}
+            </Text>
+            <Text $size={14} $weight="600">
+              {formatCurrency(product.price)}
+            </Text>
+          </ProductDetails>
+
+          <AddToCartButton>
+            <PlusCircle />
+          </AddToCartButton>
+        </Product>
+      )}
+    />
+  );
 }
