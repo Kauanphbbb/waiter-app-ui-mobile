43842b4a5715ef3d89a5a62c508f42de670dbd40
Kauanphbbb <kauanphbbb@gmail.com>
2024-03-31 15:02:21 -0300

    feat: create product modal

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,47 +1,61 @@
+import React, { useState } from 'react';
 import { FlatList } from 'react-native';
 import { products } from '../../mocks/products';
+import { Product } from '../../types/Product';
 import { formatCurrency } from '../../utils/formatCurrency';
 import { PlusCircle } from '../Icons/PlusCircle';
+import { ProductModal } from '../ProductModal';
 import { Text } from '../Text';
 import {
   AddToCartButton,
-  Product,
+  ProductContainer,
   ProductDetails,
   ProductImage,
-  Separator,
+  Separator
 } from './styles';
 
 export function Menu() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+
+  function handleOpenModal(product: Product) {
+    setIsModalVisible(true);
+    setSelectedProduct(product);
+  }
+
   return (
-    <FlatList
-      data={products}
-      keyExtractor={(product) => product._id}
-      style={{ marginTop: 32 }}
-      ItemSeparatorComponent={Separator}
-      contentContainerStyle={{ paddingHorizontal: 24 }}
-      renderItem={({ item: product }) => (
-        <Product>
-          <ProductImage
-            source={{
-              uri: `http://192.168.1.112:3001/uploads/${product.imagePath}`,
-            }}
-          />
+    <>
+      <ProductModal visible={isModalVisible} onClose={() => setIsModalVisible(false)} product={selectedProduct} />
+      <FlatList
+        data={products}
+        keyExtractor={(product) => product._id}
+        style={{ marginTop: 32 }}
+        ItemSeparatorComponent={Separator}
+        contentContainerStyle={{ paddingHorizontal: 24 }}
+        renderItem={({ item: product }) => (
+          <ProductContainer onPress={() => handleOpenModal(product)}>
+            <ProductImage
+              source={{
+                uri: `http://192.168.1.112:3001/uploads/${product.imagePath}`,
+              }}
+            />
 
-          <ProductDetails>
-            <Text $weight="600">{product.name}</Text>
-            <Text $size={14} $color="#666" style={{ marginVertical: 8 }}>
-              {product.description}
-            </Text>
-            <Text $size={14} $weight="600">
-              {formatCurrency(product.price)}
-            </Text>
-          </ProductDetails>
+            <ProductDetails>
+              <Text $weight="600">{product.name}</Text>
+              <Text $size={14} $color="#666" style={{ marginVertical: 8 }}>
+                {product.description}
+              </Text>
+              <Text $size={14} $weight="600">
+                {formatCurrency(product.price)}
+              </Text>
+            </ProductDetails>
 
-          <AddToCartButton>
-            <PlusCircle />
-          </AddToCartButton>
-        </Product>
-      )}
-    />
+            <AddToCartButton>
+              <PlusCircle />
+            </AddToCartButton>
+          </ProductContainer>
+        )}
+      />
+    </>
   );
 }
